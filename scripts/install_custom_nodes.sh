939796cb30640ed782fecca94d7c8c20c450e480
Yulu Xu <xuyulu86@126.com>
2024-06-01 16:19:28 +0800

    update script

--- scripts/install_custom_nodes.sh
+++ scripts/install_custom_nodes.sh
@@ -3,24 +3,31 @@
 PROPERTIES_FILE="custom_nodes"
 DESTINATION_FOLDER="../custom_nodes"
 
+install_requirements() {
+    if [ -d "$1/requirements.txt" ]; then
+        python3 -m pip install -r "$1/requirements.txt"
+    fi
+}
+
+update_or_clone_project(){
+    local project=$1
+    local path=$2
+
+    if [ ! -d "$DESTINATION_FOLDER/$project" ]; then
+        echo "Cloning project $project from $path..."
+        git clone "$path" "$DESTINATION_FOLDER/$project"
+    else
+        echo "Project already exists. Updating project Name: $project"
+        git --git-dir="$DESTINATION_FOLDER/$project/.git" --work-tree="$DESTINATION_FOLDER/$project" pull
+    fi
+
+    install_requirements "$DESTINATION_FOLDER/$project"
+}
+
 while IFS="=" read -r project_name git_path || [[ -n "$project_name" && -n "$git_path" ]]; do
     if [[ $project_name == \#* ]]; then
         continue
     fi
 
-    proxy_git_path="https://gh-proxy.com/$git_path"    
-
-    if [ ! -d "$DESTINATION_FOLDER/$project_name" ]; then
-        echo "Cloning project $project_name from $git_path..."
-        git clone "$proxy_git_path" "$DESTINATION_FOLDER/$project_name"
-        if [ -d "$DESTINATION_FOLDER/$project_name/requirements.txt" ]; then
-            python3 -m pip install -r "$DESTINATION_FOLDER/$project_name/requirements.txt"
-        fi
-    else
-        echo "Project already exists. Update project Name: $project_name"
-        git --git-dir="$DESTINATION_FOLDER/$project_name/.git" --work-tree="$DESTINATION_FOLDER/$project_name" pull
-        if [ -d "$DESTINATION_FOLDER/$project_name/requirements.txt" ]; then
-            python3 -m pip install -r "$DESTINATION_FOLDER/$project_name/requirements.txt" 
-        fi
-    fi
+    update_or_clone_project "$project_name" "$git_path"
 done < "$PROPERTIES_FILE"
